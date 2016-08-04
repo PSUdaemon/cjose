5a32e720d9eb4d8a8260a32259ae60b51abdaf51
Matt Miller (mamille2) <mamille2@cisco.com>
2016-08-04 11:12:19 -0600

    Release 0.4.0

--- search/functions_0.js
+++ search/functions_0.js
@@ -4,10 +4,14 @@ var searchData=
   ['cjose_5fbase64_5fencode',['cjose_base64_encode',['../base64_8h.html#a90a2def98245ac9a083e63d06e0eef1a',1,'base64.h']]],
   ['cjose_5fbase64url_5fdecode',['cjose_base64url_decode',['../base64_8h.html#aed7ef9dcf6a04cee1af0a6b9d0af7dd5',1,'base64.h']]],
   ['cjose_5fbase64url_5fencode',['cjose_base64url_encode',['../base64_8h.html#a4fa02109fd6bc4a09c432d18e776ccea',1,'base64.h']]],
+  ['cjose_5fconst_5fmemcmp',['cjose_const_memcmp',['../util_8h.html#a212b3b1dadc74ad983a5c46d6e18d404',1,'util.h']]],
   ['cjose_5ferr_5fmessage',['cjose_err_message',['../error_8h.html#a4d6aba4ba4eaeb0c72dc932325b8047f',1,'error.h']]],
   ['cjose_5fget_5falloc',['cjose_get_alloc',['../util_8h.html#a4e7fc330e51442b5aafbeba1f393b4c7',1,'util.h']]],
+  ['cjose_5fget_5falloc3',['cjose_get_alloc3',['../util_8h.html#a2ba664b68eac44005a7b0da790c0c9ba',1,'util.h']]],
   ['cjose_5fget_5fdealloc',['cjose_get_dealloc',['../util_8h.html#a29af3d34fdc8bd412aaf89b8e235cf25',1,'util.h']]],
+  ['cjose_5fget_5fdealloc3',['cjose_get_dealloc3',['../util_8h.html#ac133c0f9fd2659538d63478582473695',1,'util.h']]],
   ['cjose_5fget_5frealloc',['cjose_get_realloc',['../util_8h.html#ae2bc9cb2705fbdceb7919c5de7e27d6b',1,'util.h']]],
+  ['cjose_5fget_5frealloc3',['cjose_get_realloc3',['../util_8h.html#ab652bf6590a2fbd276c66212131546a4',1,'util.h']]],
   ['cjose_5fheader_5fget',['cjose_header_get',['../header_8h.html#aa352a3be56e50e00af6d117c7ca581dc',1,'header.h']]],
   ['cjose_5fheader_5fnew',['cjose_header_new',['../header_8h.html#a4507dd8f7840ed73c82c11c9c02a8fbb',1,'header.h']]],
   ['cjose_5fheader_5frelease',['cjose_header_release',['../header_8h.html#a78fee8f970eadd8c131c3534b8cd2bb7',1,'header.h']]],
@@ -44,6 +48,7 @@ var searchData=
   ['cjose_5fjws_5frelease',['cjose_jws_release',['../jws_8h.html#a4bb4cb0cdea31f29fce8dcd7847623ca',1,'jws.h']]],
   ['cjose_5fjws_5fsign',['cjose_jws_sign',['../jws_8h.html#a51d5e4c61df26017fe09126f28df1010',1,'jws.h']]],
   ['cjose_5fjws_5fverify',['cjose_jws_verify',['../jws_8h.html#abc5d5c8757d8144dec16ead80dfb104b',1,'jws.h']]],
+  ['cjose_5fset_5falloc_5fex_5ffuncs',['cjose_set_alloc_ex_funcs',['../util_8h.html#ac5e33cd319f15ab64a9b40e83f0e6413',1,'util.h']]],
   ['cjose_5fset_5falloc_5ffuncs',['cjose_set_alloc_funcs',['../util_8h.html#aa5c2aabebabb066e431818d7ed5393aa',1,'util.h']]],
   ['cjose_5fversion',['cjose_version',['../version_8h.html#ae4ce61c27d0fcfba9c1ada25d87d425a',1,'version.h']]]
 ];
